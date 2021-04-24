e09f7a4e5cf7f69b25567e1eb45c072de599261e
bhvdgkireetivarma <bhvdgkireetivarma123@gmail.com>
2021-04-25 00:45:10 +0530

    add student

--- teacherlogin/addstudent.php
+++ teacherlogin/addstudent.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="addstudent.css?version=51"/>
+<link rel="stylesheet" href="addstudent.css?version=81"/>
 <?php 
 
 $conn=new mysqli("localhost","root","","studentresult");
@@ -47,66 +47,160 @@ if($name!=""&&$gender!=""&&$rollnum!=""&&$bday!=""&&$phone!=""&&$email!=""&&$pas
 
 ?>
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-
-<head>
-
-</head>
+    <title>Hello, world!</title>
+  </head>
 <body>
-<div class="main">
-<div class="maine sub">
-<div class="reg">Add Student</div>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-<div class="total">
-    <div class="first">
+<div class="reg">Add Student</div>
+
+<div class="container1 my-5">
+  
+  <button type="button" class="button1" data-toggle="collapse" >Add Login Details</button>
+  <div id="demo" class="mx-3 my-3">
+      <div>
+     
+  <div class="form-group">
+    <label for="exampleInputEmail1">Student Roll Number</label>
+    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Roll Number" name="rollnumber">
+   
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" value="FakePSW" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+  </div>
 <div>
-Student Roll: <input type="number" name="rollnumber"/>
+<input type="checkbox" class="px-2" onclick="myfunction()">Show Password</input>
 </div>
-<div>
-Name: <input type="text" name="name"/>
+     </div>
+  </div>
 </div>
-<div>
-Gender: <input type="text" name="gender"/>
+
+<div class="container2 my-5" >
+  
+  <button type="button" class="button1" data-toggle="collapse" data-target="#demo2">Add Details</button>
+  <div id="demo2" class="collapse mx-3 my-3">
+      <div>
+      <div>
+
+
+      <div class="my-3">
+     
+     <div class="form-group">
+       <label for="exampleInputEmail1">Student Name</label>
+       <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  name="name">
+      
+     </div>
+     <div class="form-group">
+       <label for="exampleInputPassword1">gender</label>
+       <input type="text" class="form-control" id="exampleInputPassword1"  name="gender">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">Class</label>
+       <input type="number" class="form-control" id="exampleInputPassword1"  name="class">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">Email</label>
+       <input type="email" class="form-control" id="exampleInputPassword1"  name="email">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">BirthDate</label>
+       <input type="date" class="form-control" id="exampleInputPassword1" name="bday">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">Phone number</label>
+       <input type="varchar"   class="form-control" id="exampleInputPassword1"  name="phnum">
+     </div>
+ </div>
+  </div>
 </div>
-<div>
-Class: <input type="number" name="class"/>
+
 </div>
-<div>
-Password: <input type="password" name="password"/>
+
+
+<div class="container2 my-5" >
+  
+  <button type="button" class="button1" data-toggle="collapse" data-target="#demo3">Add Marks</button>
+  <div id="demo3" class="collapse mx-3 my-3">
+      <div>
+      <div>
+
+
+      <div class="my-3">
+     
+     <div class="form-group">
+       <label for="exampleInputEmail1">Telugu</label>
+       <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  name="telugu">
+      
+     </div>
+     <div class="form-group">
+       <label for="exampleInputPassword1">English</label>
+       <input type="number" class="form-control" id="exampleInputPassword1"  name="english">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">Hindi</label>
+       <input type="number" class="form-control" id="exampleInputPassword1"  name="hindi">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">Social</label>
+       <input type="number" class="form-control" id="exampleInputPassword1"  name="social">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">Science</label>
+       <input type="number" class="form-control" id="exampleInputPassword1" name="science">
+     </div>
+
+     <div class="form-group">
+       <label for="exampleInputPassword1">Cp</label>
+       <input type="number"   class="form-control" id="exampleInputPassword1"  name="cp">
+     </div>
+ </div>
+  </div>
 </div>
-<div>
-Email Id: <input type="email" name="email"/>
+
 </div>
-<div>
-Birthdate: <input type="date" name="bday"/>
-</div>
-<div>
-    Phone Number: <input type="varchar" name="phnum"/>
-</div>
-</div>
-<div class="second">
-    <div>Telugu: <input type="number" name="telugu"/></div>
-    <div>English: <input type="number" name="english"/></div>
-    <div>Hindi: <input type="number" name="hindi"/></div>
-    <div>Social: <input type="number" name="social"/></div>
-    <div>Science: <input type="number" name="science"/></div>
-    <div>Cp: <input type="number" name="cp"/></div>
+
     <div class="regi">
-<button type="submit" name="register">Register</button>
+<button type="submit" class="regis" name="register">Register</button>
 </div>
 <div class="errorr">
  <span><?php echo "$finalmsg";?></span>
 </div>
-   
-</div>
-</div>
-
 
 
 </form>
 </div>
 
 </div>
+<script>
+function myfunction()
+{
+    var x = document.getElementById("exampleInputPassword1");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+  }
 
+</script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
