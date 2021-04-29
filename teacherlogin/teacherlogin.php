eb69540b9b3094752d22c7fe4c79db7e3f2c9d8f
bhvdgkireetivarma <bhvdgkireetivarma123@gmail.com>
2021-04-29 10:53:46 +0530

    teacher login

--- teacherlogin/teacherlogin.php
+++ teacherlogin/teacherlogin.php
@@ -168,7 +168,7 @@ if(isset($_POST['submit22']))
 ?>
 
 <head>
-<title>Bootstrap Example</title>
+<title>Teacher Login</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
